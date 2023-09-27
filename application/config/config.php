46dd7bde24f9c4e9a952032efcd52cd69e7ee490
Your Name <you@example.com>
2023-09-27 11:29:35 +0700

    live

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ryunco';
+$config['base_url'] = 'https://muhamadarifwb.github.io/Tugas_UAS_Ryunco/';
 
 /*
 |--------------------------------------------------------------------------
